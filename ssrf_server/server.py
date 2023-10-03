dafa211d74b933b74d444c98d227c60ad3223a56
za <zakiakhmad@gmail.com>
2023-10-03 16:23:25 +0700

    Format the Python code with Black
    to have the style more standardize.
    
    [Black](https://github.com/psf/black)

--- ssrf_server/server.py
+++ ssrf_server/server.py
@@ -4,20 +4,23 @@ from bottle import route, run, template, request
 import io
 import pycurl
 
-@route('/')
+
+@route("/")
 def hello():
     return template("index.html")
 
-@route('/get_contents', method="POST")
+
+@route("/get_contents", method="POST")
 def get_contents():
-    url = request.forms.get('url')
+    url = request.forms.get("url")
     curl = pycurl.Curl()
     curl.setopt(pycurl.URL, url)
     curl.setopt(pycurl.TIMEOUT, 15)
     buffer = io.BytesIO()
     curl.setopt(curl.WRITEDATA, buffer)
     curl.perform()
-    body = buffer.getvalue().decode('utf-8')
+    body = buffer.getvalue().decode("utf-8")
     return template("get.html", contents=body)
 
-run(host='0.0.0.0', port=8080, debug=True)
+
+run(host="0.0.0.0", port=8080, debug=True)
